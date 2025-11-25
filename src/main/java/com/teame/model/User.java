5f9223a2d47a8e2c19e1b1d6b59fb9e4a021a730
Deshan-Cloud99x <smddsenyake@gmail.com>
2025-11-26 00:01:44 +0530

    User.java

---
+++ src/main/java/com/teame/model/User.java
@@ -0,0 +1,56 @@
+package com.teame.model;
+
+/**
+ * Base class for all system users (Participant, Organizer).
+ * Contains shared identity-related fields.
+ */
+public class User {
+
+    protected String id;      // unique identifier (email or system-generated)
+    protected String name;    // full name
+    protected String email;   // login email
+
+    public User() {
+    }
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // --- Getters & Setters ---
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // --- Utility ---
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
