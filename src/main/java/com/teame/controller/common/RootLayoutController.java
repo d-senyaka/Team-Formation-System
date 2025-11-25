e99c795b9958ffec38239e4f6478d8ba394cafd8
Deshan-Cloud99x <smddsenyake@gmail.com>
2025-11-26 05:06:03 +0530

    participant_login.fxml + ParticipantLoginController
    Built login screen in Scene Builder:
    
    Fields: TextField emailField, TextField idField
    
    Buttons: Login, Create Account
    
    Controller:
    
    Validate non-empty + email format.
    
    Use ParticipantService.findOrCreate(...)
    
    Store result in ParticipantSession.
    
    Navigate to survey screen.

--- src/main/java/com/teame/controller/common/RootLayoutController.java
+++ src/main/java/com/teame/controller/common/RootLayoutController.java
@@ -1,4 +1,44 @@
 package com.teame.controller.common;
 
+import com.teame.config.AppConfig;
+import com.teame.controller.participant.ParticipantLoginController;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.Node;
+
+import java.io.IOException;
+
 public class RootLayoutController {
+
+    @FXML
+    private BorderPane rootLayout; // make sure your root_layout.fxml has fx:id="rootLayout"
+
+    private final AppConfig appConfig = new AppConfig();
+
+    public void initialize() {
+        // Optionally show login on startup
+        showParticipantLogin();
+    }
+
+    public void showParticipantLogin() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(
+                    "/com/teame/fxml/participant/participant_login.fxml"));
+            Node view = loader.load();
+
+            ParticipantLoginController controller = loader.getController();
+            controller.init(appConfig, this);
+
+            rootLayout.setCenter(view);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // This will be used by the login controller
+    public void showParticipantSurvey() {
+        // We will implement this in Phase 6.3
+    }
 }
