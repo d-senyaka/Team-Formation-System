51f2a3382ec0ef4d951a41918c96670fc5c58986
Deshan-Cloud99x <smddsenyake@gmail.com>
2025-11-26 06:06:24 +0530

    Core Organizer Context & Wiring (backend setup)
    Created shared state + services so all organizer screens see the same data.
    
    OrganizerContext (or OrganizerDataContext)
    
    List<Participant> cleanedParticipants
    
    List<InvalidRow> invalidRows
    
    List<Team> formedTeams
    
    maybe String sourceFilePath
    
    Extended AppConfig:
    
    add OrganizerContext
    
    add ConcurrencyService
    
    add TeamFormationService
    
    add TeamRepository / CsvTeamRepository
    
    Added navigation methods stubs in RootLayoutController:
    
    showOrganizerDashboard()
    
    showOrganizerDataSource()
    
    showOrganizerInvalidRows()
    
    showOrganizerTeamFormation()
    
    showOrganizerViewTeams()

--- src/main/java/com/teame/config/AppConfig.java
+++ src/main/java/com/teame/config/AppConfig.java
@@ -6,10 +6,20 @@ import com.teame.service.ParticipantSession;
 import com.teame.service.PersonalityService;
 import com.teame.service.ValidationService;
 
+import com.teame.repository.CsvTeamRepository;
+import com.teame.repository.TeamRepository;
+import com.teame.concurrency.ConcurrencyService;
+import com.teame.service.TeamFormationService;
+import com.teame.service.OrganizerContext;
+
+
 public class AppConfig {
 
     private static final String PARTICIPANTS_FILE =
             "src/main/resources/com/teame/data/participants_saved.csv";
+
+    private static final String FORMED_TEAMS_FILE =
+            "src/main/resources/com/teame/data/formed_teams.csv";
 
     private final ParticipantSession participantSession = new ParticipantSession();
     private final ValidationService validationService = new ValidationService();
@@ -18,6 +28,15 @@ public class AppConfig {
             new CsvParticipantRepository(validationService);
     private final ParticipantService participantService =
             new ParticipantService(participantRepository, PARTICIPANTS_FILE);
+
+
+    // ---- New for organizer side ----
+    private final OrganizerContext organizerContext = new OrganizerContext();
+    private final ConcurrencyService concurrencyService =
+            new ConcurrencyService(personalityService, validationService);
+    private final TeamFormationService teamFormationService = new TeamFormationService();
+    private final TeamRepository teamRepository =
+            new CsvTeamRepository(FORMED_TEAMS_FILE);
 
     public ParticipantSession getParticipantSession() {
         return participantSession;
@@ -33,5 +52,23 @@ public class AppConfig {
 
     public ParticipantService getParticipantService() {
         return participantService;
+    }
+
+
+    // Organizer side getters
+    public OrganizerContext getOrganizerContext() {
+        return organizerContext;
+    }
+
+    public ConcurrencyService getConcurrencyService() {
+        return concurrencyService;
+    }
+
+    public TeamFormationService getTeamFormationService() {
+        return teamFormationService;
+    }
+
+    public TeamRepository getTeamRepository() {
+        return teamRepository;
     }
 }
