991b9392d2de9869ecda189c994d98268b9dd239
Deshan-Cloud99x <smddsenyake@gmail.com>
2025-11-28 23:37:10 +0530

    packages for module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,22 @@
 module org.oop.teameintelligentteamformation {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
+    requires javafx.graphics;
+    requires javafx.base;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires eu.hansolo.tilesfx;
-
+    // FXML controllers
+    opens com.teame to javafx.fxml;
     opens com.teame.controller.common to javafx.fxml;
-    opens com.teame.controller.organizer to javafx.fxml;
     opens com.teame.controller.participant to javafx.fxml;
+    opens com.teame.controller.organizer to javafx.fxml;
 
+    // Model classes used by TableView / PropertyValueFactory (Participant, User, etc.)
+    opens com.teame.model to javafx.base, javafx.fxml;
+
+    // (optional) exports – only if you need them outside the module
     exports com.teame;
+    exports com.teame.controller.common;
+    exports com.teame.controller.participant;
+    exports com.teame.controller.organizer;
+    exports com.teame.model;
 }
